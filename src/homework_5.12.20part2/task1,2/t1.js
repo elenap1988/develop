41baba5c3603a99b589450e5c982add5d1080a8f
Lena Prokofieva <elenap1988@tut.by>
2020-12-03 17:43:14 +0300

    homework5.12.20part2
    Co-Authored-By: Maksim <58484400+MaksimFalei@users.noreply.github.com>

---
+++ src/homework_5.12.20part2/task1,2/t1.js
@@ -0,0 +1,24 @@
+//task1
+
+// let element = prompt('введите название элемента');
+// console.log(element);
+
+// let content = prompt('введите содержимое элемента');
+// console.log(content);
+
+// 	element2 = document.createElement(element);
+// 	document.body.append(element2);
+// 	element2.innerHTML = content;
+
+//task2
+
+// let parent = prompt('введите название родительского элемента');
+// let child = prompt('введите название дочернего элемента');
+// let сontent = prompt('введите текст дочернего элемента');
+
+// parent2 = document.createElement(parent);
+// document.body.append(parent2);
+// child2 = document.createElement(child);
+// parent2.appendChild(child2);
+// console.log(child2);
+// child2.innerHTML = сontent;
